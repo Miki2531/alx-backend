1c710660e4f0f16cf3fbdcd42e643fc75948b52c
Miki2531 <mikiyasshemelis46@gmail.com>
2023-07-23 22:19:33 +0300

    returns a dictionary

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -8,6 +8,8 @@ from typing import Dict, List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
+        page -> The current page number(1-indexed).
+        page_size -> The nubmer of items to display on each page.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
